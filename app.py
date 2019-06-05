b5774f00075c41e59d57e5f69831cf9d9b9c967d
Kevin Walton <kevywalton@gmail.com>
2019-06-05 14:59:38 +0000

    ???

--- app.py
+++ app.py
@@ -1,75 +1,91 @@
 import os
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, redirect, request, url_for, request
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 
 app = Flask(__name__)
-
-
-app.config["MONGO_DBNAME"] = "task_manager"
-app.config["MONGO_URI"] = "mongodb+srv://root:mongoDB2019@myfirstcluster-oyutj.mongodb.net/task_manager?retryWrites=true&w=majority"
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 mongo = PyMongo(app)
 
-@app.route("/")
-@app.route("/get_tasks")
+@app.route('/')
+@app.route('/get_tasks')
 def get_tasks():
-    _tasks = mongo.db.tasks.find()
-    task_list = [task for task in _tasks]
-    return render_template("tasks.html", tasks = task_list)
+    return render_template("tasks.html", 
+                           tasks=mongo.db.tasks.find())
 
 
-@app.route("/add_task")
+@app.route('/add_task')
 def add_task():
-    _categories = mongo.db.categories.find()
-    category_list = [category for category in _categories]
-    return render_template("addtask.html", categories = category_list)
+    return render_template('addtask.html',
+        categories=mongo.db.categories.find())
 
 
-@app.route("/insert_task", methods=["POST"])
+@app.route('/insert_task', methods=['POST'])
 def insert_task():
-    tasks = mongo.db.tasks
+    tasks =  mongo.db.tasks
     tasks.insert_one(request.form.to_dict())
-    return redirect(url_for("get_tasks"))
+    return redirect(url_for('get_tasks'))
 
 
-@app.route("/edit_task/<task_id>")
+@app.route('/edit_task/<task_id>')
 def edit_task(task_id):
-    the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
-    all_categories = mongo.db.categories.find()
-    return render_template("edittask.html", category=all_categories, task=the_task)
+    the_task =  mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    all_categories =  mongo.db.categories.find()
+    return render_template('edittask.html', task=the_task,
+                           categories=all_categories)
 
 
-@app.route("/update_task/<task_id>", methods=["POST"])
+@app.route('/update_task/<task_id>', methods=["POST"])
 def update_task(task_id):
     tasks = mongo.db.tasks
-    tasks.update( {"_id": ObjectId(task_id)},
+    tasks.update( {'_id': ObjectId(task_id)},
     {
-        'task_name': request.form.get('task_name'),
-        'category_name': request.form.get('category_name'),
+        'task_name':request.form.get('task_name'),
+        'category_name':request.form.get('category_name'),
         'task_description': request.form.get('task_description'),
         'due_date': request.form.get('due_date'),
-        'is_urgent': request.form.get('is_urgent')
+        'is_urgent':request.form.get('is_urgent')
     })
-    return redirect(url_for("get_tasks"))
-    
+    return redirect(url_for('get_tasks'))
+
 
 @app.route('/delete_task/<task_id>')
 def delete_task(task_id):
     mongo.db.tasks.remove({'_id': ObjectId(task_id)})
     return redirect(url_for('get_tasks'))
-    
 
-@app.route("/get_categories")
+
+@app.route('/get_categories')
 def get_categories():
-    return render_template("categories.html", 
-            categories=mongo.db.categories.find())
+    return render_template('categories.html',
+                           categories=mongo.db.categories.find())
+                           
 
- 
-if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
+@app.route('/edit_category/<category_id>')
+def edit_category(category_id):
+    return render_template('editcategory.html',
+           category=mongo.db.categories.find_one(
+           {'_id': ObjectId(category_id)}))
+
+
+@app.route('/update_category/<category_id>', methods=['POST'])
+def update_category(category_id):
+    mongo.db.categories.update(
+        {'_id': ObjectId(category_id)},
+        {'category_name': request.form.get('category_name')})
+    return redirect(url_for('get_categories'))
+
+
+@app.route('/delete_category/<category_id>')
+def delete_category(category_id):
+    mongo.db.categories.remove({'_id': ObjectId(category_id)})
+    return redirect(url_for('get_categories'))
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
             debug=True)
-
-
